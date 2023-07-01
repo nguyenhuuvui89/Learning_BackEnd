2ab45675ffafc1c07d865d5cf4de2c5fad7badd6
Vincent <nguyenhuuvui89@gmail.com>
2023-06-30 23:51:08 -0230

    add mailchimp

--- Signup-Newsletter/app.js
+++ Signup-Newsletter/app.js
@@ -1,3 +1,6 @@
+// Mailchimp API Key: 10b2ebcae12628dea3fc36d4ffcff16f-us11
+// List ID: d7973ff559
+
 /* eslint-disable no-unused-vars */
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -16,7 +19,32 @@ app.post('/', (req, res) => {
   const fName = req.body.firstName;
   const lName = req.body.lastName;
   const emailEnter = req.body.email;
-  console.log(fName, lName, emailEnter);
+  const data = {
+    members: [
+      {
+        email_address: emailEnter,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: fName,
+          LNAME: lName,
+        },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);
+  const options = {
+    method: 'POST',
+    auth: 'VincentN:10b2ebcae12628dea3fc36d4ffcff16f-us11',
+  };
+  const reques = https.request('https://us11.api.mailchimp.com/3.0/lists/d7973ff559', options, (response) => {
+    response.on('data', (dat) => {
+      if (response.statusCode === 200) {
+        res.sendFile(`${__dirname}/success.html`);
+      }
+    });
+  });
+  reques.write(jsonData);
+  reques.end();
 });
 
 app.listen(3000, () => {
